30b06bb2099207269d11a14382acbae5e3e9d1e8
ruksan_emirali <ruksanemirali@gmail.com>
2024-11-01 17:04:56 +0000

    Sets up basic frontend routing, issues with sass

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb4febecd4d3c171207cbb22c0cb863b2e4b1b47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b8cb5f4f59c5bf6ec07700cda956d352ceebb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/src/Pages/Students/Students.tsx
@@ -0,0 +1,10 @@
+import "./Students.scss"
+
+const Students: React.FC = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Students
