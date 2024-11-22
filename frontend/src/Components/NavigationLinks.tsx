b56b8451d601069b9934408edda2c7a95d82bd98
ruksan_emirali <ruksanemirali@gmail.com>
2024-11-22 15:37:04 +0000

    removes commented out old code

--- frontend/src/Components/NavigationLinks.tsx
+++ frontend/src/Components/NavigationLinks.tsx
@@ -26,25 +26,6 @@ const NavigationLinks: React.FC = () => {
     </Navbar>
     </>
 
-
-    
-    // <ul className="navbar-nav mb-2 mb-lg-0 d-flex w-100 justify-content-around" data-testid="navigationLinks">
-    //   <li className="nav-item">
-    //     <Link to="/" className="nav-link" data-testid="homeLink">
-    //       Home
-    //     </Link>
-    //   </li>
-    //   <li className="nav-item">
-    //     <Link to="/students" className="nav-link">
-    //       Student List
-    //     </Link>
-    //     <li className="nav-item">
-    //       <Link to="/students/search" className="nav-link">
-    //         Search Students
-    //       </Link>
-    //     </li>
-    //   </li>
-    // </ul>
   )
 }
 
